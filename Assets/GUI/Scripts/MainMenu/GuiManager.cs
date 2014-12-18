5a6c2635227f67a8fa6357500b3db991e0ec7818
Mats Wilhelm <matschew@web.de>
2014-12-18 20:50:50 +0100

    Gui erstellt

---
+++ Assets/GUI/Scripts/MainMenu/GuiManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiManager : MonoBehaviour {
+
+	public void CloseGame()
+	{
+		Application.Quit ();
+	}
+
+	public void LoadMultiplayerMenu()
+	{
+		Application.LoadLevel ("MultiplayerMenu");
+	}
+
+	public void LoadOptionsMenu()
+	{
+		Application.LoadLevel ("OptionsMenu");
+	}
+
+	public void LoadSingleplayerMenu()
+	{
+		Application.LoadLevel ("SingleplayerMenu");
+	}
+
+	public void loadMainMenu()
+	{
+		Application.LoadLevel ("MainMenu");
+	}
+
+	public void loadSingleplayerGame()
+	{
+		Application.LoadLevel ("Game");
+		Debug.Log ("Lade Einzelspieler...");
+	}
+
+	public void loadMultiplayerGameAsServer()
+	{
+		Application.LoadLevel ("Game");
+		Debug.Log ("Erstelle Server für Multiplayer");
+	}
+
+	public void loadMultiplayerGameAsClient()
+	{
+		Application.LoadLevel ("Game");
+		Debug.Log ("Verbinde zu Server für Multiplayer");
+	}
+}
