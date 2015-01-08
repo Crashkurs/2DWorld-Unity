2292fcaa81ff674b26729007af1d65fc02da2556
Mats <matschew@web.de>
2015-01-08 08:59:04 +0100

    hut

---
+++ Assets/TestScripts/FreeRoamCamera.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FreeRoamCamera : MonoBehaviour {
+
+	Vector3 rotateDragStart;
+	float rotateSpeed = .2f;
+	float translateSpeed = 3;
+
+	public enum Button : int { Left = 0, Right = 1, Middle = 2, None = 3 }
+
+	float xRotation = 0;
+	float yRotation = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown((int)Button.Right)) {
+			rotateDragStart = Input.mousePosition;
+		}
+
+		if(Input.GetMouseButton((int)Button.Right)) {
+			xRotation = Input.mousePosition.x - rotateDragStart.x;
+			yRotation = Input.mousePosition.y - rotateDragStart.y;
+		} else {
+			xRotation = 0;
+			yRotation = 0;
+		}
+
+
+		transform.localRotation *= Quaternion.AngleAxis(xRotation*rotateSpeed*Time.deltaTime, Vector3.up);
+		transform.localRotation *= Quaternion.AngleAxis(yRotation*rotateSpeed*Time.deltaTime, Vector3.left);
+		transform.localRotation = Quaternion.LookRotation(transform.forward, Vector3.up);
+		
+		transform.position += transform.forward*translateSpeed*Input.GetAxis("Vertical")*Time.deltaTime;
+		transform.position += transform.right*translateSpeed*Input.GetAxis("Horizontal")*Time.deltaTime;
+
+		transform.position += new Vector3(0,Input.GetAxis("Mouse ScrollWheel")*translateSpeed,0);
+	}
+}
---
+++ Assets/TestScripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+
+public class GameManager : MonoBehaviour
+{
+
+	public List<GameObject> selectedUnits = new List<GameObject> ();
+	
+	public GridGraph graph;
+
+	private Camera camera;
+	
+	private float timer = 1f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		selectedUnits = new List<GameObject> ();
+		camera = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		graph = AstarPath.active.astarData.gridGraph;
+		switchToRTSMode();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.LeftAlt)) 
+		{
+			if (Configuration.Properties.isRPGMode) 
+			{
+				switchToRTSMode ();
+			} else {
+				switchToRPGMode ();
+			}
+		}
+		timer -= Time.deltaTime;
+		if(timer <= 0)
+		{
+			graph.center = camera.transform.position;
+			graph.center.y = 0;
+			AstarPath.active.Scan();
+			timer = 1f;
+			Debug.Log("Graph updated");
+		}
+	}
+
+	public void addSelectedUnit (GameObject unit)
+	{
+		if (!selectedUnits.Contains (unit)) 
+		{
+			selectedUnits.Add (unit);
+		}
+	}
+
+	public void clearSelectedUnits ()
+	{
+		selectedUnits.Clear ();
+	}
+
+	public List<GameObject> getSelectedUnits ()
+	{
+		return selectedUnits;
+	}
+
+	public void switchToRPGMode ()
+	{
+		Configuration.Properties.isRPGMode = true;
+		camera.GetComponent<BirdCamera>().enabled = false;
+		camera.GetComponent<SmoothFollow>().enabled = true;
+		camera.isOrthoGraphic = true;
+		camera.transform.rotation = Quaternion.Euler(40, 180, 0);
+		camera.orthographicSize = 1f;
+		clearSelectedUnits();
+	}
+
+	public void switchToRTSMode ()
+	{
+		Configuration.Properties.isRPGMode = false;
+		camera.GetComponent<BirdCamera>().enabled = true;
+		camera.GetComponent<SmoothFollow>().enabled = false;
+		camera.GetComponent<BirdCamera>().panToTarget(camera.GetComponent<SmoothFollow>().target.gameObject);
+		camera.isOrthoGraphic = true;
+		camera.orthographicSize = 2f;
+		camera.transform.rotation = Quaternion.Euler(60, 180, 0);
+	}
+}
